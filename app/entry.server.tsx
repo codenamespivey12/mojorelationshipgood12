8495ace5b01745e49fa3c4f07d196fcef40a635a
Jacob Ambroz <codenamespivey@sixtyoneeightyai.com>
2025-07-22 07:58:16 -0500

    Fix Clerk Secret Key Environment Variable Error

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,10 +1,11 @@
-import 'dotenv/config';
-
 /**
  * By default, Remix will handle generating the HTTP Response for you.
  * You are free to delete this file if you'd like to, but if you ever want it revealed again, you can run `npx remix reveal` ✨
  * For more information, see https://remix.run/file-conventions/entry.server
  */
+
+// Load environment variables first, before any other imports
+import 'dotenv/config';
 
 import { PassThrough } from "node:stream";
 
